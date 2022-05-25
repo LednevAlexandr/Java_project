f46455a14c97c0680d4a6ab8b077585c1d4347b4
alex <you@example.com>
2022-05-25 21:50:37 +0300

    fist commit

---
+++ program.java
@@ -0,0 +1,13 @@
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        System.out.println(sum(5,7));
+                }
+                static public int sum(int a,int b) {
+                    return a+b;
+                }
+            }
+    
+    
