20939e70a1dc9c7048b06a9631bff6a727fbf36f
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-06-09 22:57:26 +0300

    Update knight's move.java

--- knight's move.java
+++ knight's move.java
@@ -1,5 +1,7 @@
 public class knight_move {
 	static int globalCount = 1;
+	static int y1;
+	static int x1;
 
 	public static void main(String[] args) {
 
@@ -8,10 +10,16 @@ public class knight_move {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
 				board[i][j] = 1;
-				if (!method(board, step, i, j, 0, 0)) {
-					board[i][j] = 0;
+				if (!method(board, step, i, j, 0, 0)) { // заходим с текущими i и j
+					// обнуление масива перед следующим начальным положением
+					for (int e = 0; e < board.length; e++) {
+						for (int t = 0; t < board.length; t++) {
+							board[e][t] = 0;
+						}
+					}
 					continue;
 				} else {
+					// печатаем масив если True, т.е. globalCount достиг 26
 					for (int k = 0; k < board.length; k++) {
 						for (int l = 0; l < board.length; l++) {
 							System.out.print(board[k][l] + " ");
@@ -27,21 +35,33 @@ public class knight_move {
 
 	public static boolean method(int[][] board, int[][] step, int rawBoard, int collBoard, int rawstep, int collstep) {
 
-		if (rawstep > step.length - 1)
+		if (rawstep > step.length - 1) {
+			// если в step шаги закончились, значит count = 1 и выходим в main, чтобы
+			// передвинуть начальное положение.
+			globalCount = 1;
 			return false;
-		if (globalCount == 25)
+		}
+		if (globalCount > 25)
 			return true;
-		int y1 = rawBoard + step[rawstep][collstep];
-		int x1 = collBoard + step[rawstep][collstep + 1];
+		y1 = rawBoard + step[rawstep][collstep];
+		x1 = collBoard + step[rawstep][collstep + 1];
 		if (y1 > board.length - 1 || x1 > board.length - 1 || y1 < 0 || x1 < 0) {
-			method(board, step, rawBoard, collBoard, rawstep + 1, collstep);
+			y1 = y1 - step[rawstep][collstep];
+			x1 = x1 - step[rawstep][collstep + 1];
+			return method(board, step, rawBoard, collBoard, rawstep + 1, collstep); // следующий ход с предыдущей
+																					// координаты
 		}
 		if (board[y1][x1] == 0) {
 			board[y1][x1] = globalCount + 1;
-			method(board, step, y1, x1, rawstep, collstep);
+			globalCount++;// увеличиваем
+			return method(board, step, y1, x1, rawstep, collstep);// y1 x1 - новый rawBoard и collBoard, идем дальше по
+																	// тому же ходу
 		} else {
-			method(board, step, rawBoard, collBoard, rawstep + 1, collstep);
+			y1 = y1 - step[rawstep][collstep];
+			x1 = x1 - step[rawstep][collstep + 1];
+			return method(board, step, rawBoard, collBoard, rawstep + 1, collstep); // следующий ход с предыдущей
+																					// координаты
 		}
-		return false;
+
 	}
 }
