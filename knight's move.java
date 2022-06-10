80aa483355b95b5a181e9ba64b09f696ac5cb74e
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-06-10 19:10:23 +0300

    Update knight's move.java

--- knight's move.java
+++ knight's move.java
@@ -1,17 +1,18 @@
 import java.util.Arrays;
+
 public class Main {
-static int moveNum = 1;
-static int[][] board = new int[5][5];
-static int[][] moves = { { -2, 1 }, { -2, -1 }, { 2, 1 }, { 2, -1 }, { -1, 2 }, { -1, -2 }, { 1, 2 }, { 1, -2 } };
-static int maxMoves =25;
+	static int moveNum = 1;
+	static int[][] board = new int[5][5];
+	static int[][] moves = { { -2, 1 }, { -2, -1 }, { 2, 1 }, { 2, -1 }, { -1, 2 }, { -1, -2 }, { 1, 2 }, { 1, -2 } };
+	static int maxMoves = 25;
+
 	public static void main(String[] args) {
-		
+
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
-				if (!findPath(i,j,moveNum)) {
-				    continue;
-				}
-				else {
+				if (!findPath(i, j, moveNum)) {
+					continue;
+				} else {
 					for (int k = 0; k < board.length; k++) {
 						for (int l = 0; l < board.length; l++) {
 							System.out.print(board[k][l] + " ");
@@ -24,26 +25,27 @@ static int maxMoves =25;
 		}
 	}
 
-    public static boolean findPath(int curX, int curY, int moveNum) {
-             board[curX][curY]=moveNum;
-             if (moveNum >= maxMoves) return true;
+	public static boolean findPath(int curX, int curY, int moveNum) {
+		board[curX][curY] = moveNum;
+		if (moveNum >= maxMoves)
+			return true;
 
-            for (int i = 0;i<8;i++){
-                int nextX = curX + moves[i][0];
-                int nexty = curY + moves[i][1];
-                if (isPossibleMove(nextX,nexty) && findPath(nextX, nexty, moveNum+1)){
-                    return true;
-                }
-            }
-            board[curX][curY]=0;
-		    return false;
-}
-    public static boolean isPossibleMove(int curX,int curY){
-        if (curX > board.length-1 || curY > board.length-1 || curX < 0 || curY < 0){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
+		for (int i = 0; i < 8; i++) {
+			int nextX = curX + moves[i][0];
+			int nexty = curY + moves[i][1];
+			if (isPossibleMove(nextX, nexty) && findPath(nextX, nexty, moveNum + 1)) {
+				return true;
+			}
+		}
+		board[curX][curY] = 0;
+		return false;
+	}
+
+	public static boolean isPossibleMove(int curX, int curY) {
+		if (curX > board.length - 1 || curY > board.length - 1 || curX < 0 || curY < 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
