96ed60342d92face5dc82fd5c3ae157149d1281a
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-06-10 14:32:03 +0300

    Update knight's move.java

--- knight's move.java
+++ knight's move.java
@@ -1,67 +1,49 @@
-public class knight_move {
-	static int globalCount = 1;
-	static int y1;
-	static int x1;
-
+import java.util.Arrays;
+public class Main {
+static int moveNum = 1;
+static int[][] board = new int[5][5];
+static int[][] moves = { { -2, 1 }, { -2, -1 }, { 2, 1 }, { 2, -1 }, { -1, 2 }, { -1, -2 }, { 1, 2 }, { 1, -2 } };
+static int maxMoves =25;
 	public static void main(String[] args) {
-
-		int[][] board = new int[5][5];
-		int[][] step = { { -2, 1 }, { -2, -1 }, { 2, 1 }, { 2, -1 }, { -1, 2 }, { -1, -2 }, { 1, 2 }, { 1, -2 } };
+		
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
-				board[i][j] = 1;
-				if (!method(board, step, i, j, 0, 0)) { // заходим с текущими i и j
-					// обнуление масива перед следующим начальным положением
-					for (int e = 0; e < board.length; e++) {
-						for (int t = 0; t < board.length; t++) {
-							board[e][t] = 0;
-						}
-					}
-					continue;
-				} else {
-					// печатаем масив если True, т.е. globalCount достиг 26
+				if (!findPath(i,j,moveNum)) {
+				    continue;
+				}
+				else {
 					for (int k = 0; k < board.length; k++) {
 						for (int l = 0; l < board.length; l++) {
 							System.out.print(board[k][l] + " ");
 						}
 						System.out.println();
-						break;
 					}
+					return;
 				}
 			}
 		}
-		System.out.println("Решения нет");
 	}
 
-	public static boolean method(int[][] board, int[][] step, int rawBoard, int collBoard, int rawstep, int collstep) {
+    public static boolean findPath(int curX, int curY, int moveNum) {
+             board[curX][curY]=moveNum;
+             if (moveNum >= maxMoves) return true;
 
-		if (rawstep > step.length - 1) {
-			// если в step шаги закончились, значит count = 1 и выходим в main, чтобы
-			// передвинуть начальное положение.
-			globalCount = 1;
-			return false;
-		}
-		if (globalCount > 25)
-			return true;
-		y1 = rawBoard + step[rawstep][collstep];
-		x1 = collBoard + step[rawstep][collstep + 1];
-		if (y1 > board.length - 1 || x1 > board.length - 1 || y1 < 0 || x1 < 0) {
-			y1 = y1 - step[rawstep][collstep];
-			x1 = x1 - step[rawstep][collstep + 1];
-			return method(board, step, rawBoard, collBoard, rawstep + 1, collstep); // следующий ход с предыдущей
-																					// координаты
-		}
-		if (board[y1][x1] == 0) {
-			board[y1][x1] = globalCount + 1;
-			globalCount++;// увеличиваем
-			return method(board, step, y1, x1, rawstep, collstep);// y1 x1 - новый rawBoard и collBoard, идем дальше по
-																	// тому же ходу
-		} else {
-			y1 = y1 - step[rawstep][collstep];
-			x1 = x1 - step[rawstep][collstep + 1];
-			return method(board, step, rawBoard, collBoard, rawstep + 1, collstep); // следующий ход с предыдущей
-																					// координаты
-		}
-
-	}
+            for (int i = 0;i<8;i++){
+                int nextX = curX + moves[i][0];
+                int nexty = curY + moves[i][1];
+                if (isPossibleMove(nextX,nexty) && findPath(nextX, nexty, moveNum+1)){
+                    return true;
+                }
+            }
+            board[curX][curY]=0;
+		    return false;
+}
+    public static boolean isPossibleMove(int curX,int curY){
+        if (curX > board.length-1 || curY > board.length-1 || curX < 0 || curY < 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 }
