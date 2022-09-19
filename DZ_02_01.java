0de4194a33acba28a2181aee1d5d32fbdffbe1b9
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-09-19 10:26:27 +0300

    Create DZ_02_01.java

---
+++ DZ_02_01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		double a = PrintNum();
+		System.out.println(a);
+	}
+	public static double PrintNum(){
+            try {
+                Scanner in = new Scanner(System.in);
+                System.out.print("Введите дробное число: ");
+                double num = in.nextDouble();
+                return num;
+            } catch(RuntimeException e) {
+                    Scanner in = new Scanner(System.in);
+                    System.out.print("Введите дробное число: ");
+                    double num = in.nextDouble();
+                    return num;
+                }
+                
+	}
+	
+}
